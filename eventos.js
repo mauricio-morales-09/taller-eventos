18440d3992d86b809d2adec872ab8fecb2467e1c
mauricio-morales <moralesmauricio09@outlook.com>
2023-09-03 15:23:59 -0300

    Update eventos.js

--- eventos.js
+++ eventos.js
@@ -3,3 +3,14 @@ const contenedor = document.querySelector('.contenedor');
 contenedor.addEventListener('click', function() {
     alert('Hola! Soy el div');
 });
+
+const boton = document.querySelector('button');
+
+boton.addEventListener('click', function(event) {
+    event.stopPropagation(); // Evita que el clic se propague al div
+    alert('Hola desde el botón');
+});
+
+
+
+
